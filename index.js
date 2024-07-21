2063dc3ffae0c3bde31b23cb0c3c9494eb57eb1b
Olusegun .O. ADELEKE <oadeleke123@gmail.com>
2024-07-21 12:44:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ import { Probot } from "probot";
 import { exec } from "child_process";
 import util from 'util';
 import fs from 'fs/promises';
+import dotenv from 'dotenv';
 
-
+dotenv.config();
 const execPromise = util.promisify(exec);
 
 export default (app) => {
@@ -15,49 +16,36 @@ export default (app) => {
     const status = context.repo({
       sha: pull_request.head.sha,
       state: 'pending',
-      context: 'CI/CD',
+      context: 'DEPLOY',
       description: 'Automation PR bot is running'
-
+      
     });
     await context.octokit.repos.createCommitStatus(status);
-
-
-    if (action == 'opened') {
-        // Initial comment
-        const comment = context.issue({
-            body: 'Deployment is starting... '
-        });
-        await context.octokit.issues.createComment(comment);
-    }  else {
-        // Initial comment
-        const comment = context.issue({
-            body: 'Deployment is restarting... '
-        });
-        await context.octokit.issues.createComment(comment);
-    }
 
     try {
       // Run the deployment script
       const branchName = context.payload.pull_request.head.ref;
       const pullRequestNumber = pull_request.number;
       const forkedRepoUrl = context.payload.pull_request.head.repo.clone_url;
-      const { stdout, stderr } = await execPromise(`chmod +x ./deploy.sh && sudo ./deploy.sh ${branchName} ${pullRequestNumber} ${forkedRepoUrl}`); 
+      const { stdout, stderr } = await execPromise(`chmod +x ./deploy.sh && sudo ./deploy.sh ${branchName} ${pullRequestNumber} ${forkedRepoUrl}`);
       console.log('Deployment output:', stdout);
       const port = await fs.readFile('port.txt', 'utf8');
-      const deploymentUrl = `http://91.229.239.118:${port.trim()}`;
-
-
+      const deploymentIp = process.env.DEPLOYMENT_IP;
+      const deploymentUrl = `http://${deploymentIp}:${port.trim()}`;
+      
       if (stderr) {
         console.error('Deployment errors:', stderr);
       }
-
+      
       const currentDate = new Date().toUTCString();
-
       const markdownComment = `
-      | Name | Status | Preview |Updated (UTC) |
-      |------|--------|---------|--------------|
-      | ${context.payload.pull_request.head.ref} | ✅ Ready ([Inspect](${deploymentUrl})) | [Visit Preview](${deploymentUrl}) | ${currentDate} |    
-      `;
+Deployment Successful
+
+| Branch | Status | Preview |Updated (UTC) |
+|--------|--------|---------|--------------|
+| ${context.payload.pull_request.head.ref} | ✅ Ready ([Inspect](${deploymentUrl})) | [Visit Preview](${deploymentUrl}) | ${currentDate} |
+
+`; 
       // Update comment with success
       const comment = context.issue({
         body: markdownComment
@@ -68,7 +56,7 @@ export default (app) => {
       const status = context.repo({
         sha: pull_request.head.sha,
         state: 'success',
-        context: 'CI/CD',
+        context: 'DEPLOY',
         description: 'PR deployed successfully',
         target_url: `${deploymentUrl}`
       });
@@ -80,8 +68,17 @@ export default (app) => {
     } catch (error) {
       // Update comment with failure
       console.error('Deployment errors:', error);
+      const currentDate = new Date().toUTCString();
+      const markdownComment = `
+Deployment Failed
+
+| Branch | Status | Preview |Updated (UTC) |
+|--------|--------|---------|--------------|
+| ${context.payload.pull_request.head.ref} | ❌ Ready | | ${currentDate} |
+
+`;
       const comment = context.issue({
-        body: 'Deployment failed'
+        body: markdownComment
       });
       await context.octokit.issues.createComment(comment);
 
@@ -89,9 +86,9 @@ export default (app) => {
       const status = context.repo({
         sha: pull_request.head.sha,
         state: 'failure',
-        context: 'CI/CD',
+        context: 'DEPLOY',
         description: 'PR deployment failed'
-
+       
       });
       await context.octokit.repos.createCommitStatus(status);
     }
@@ -106,7 +103,7 @@ export default (app) => {
       const status = context.repo({
         sha: pull_request.head.sha,
         state: 'success',
-        context: 'CI/CD',
+        context: 'MERGE',
         description: 'PR merged successfully'
 
       });
@@ -114,16 +111,11 @@ export default (app) => {
 
       // Post a PR Merged success message
       const comment = context.issue({
-        body: 'Congrats, The Pull Request has been merged!'
+        body: 'The Pull Request has been merged!'
       });
       await context.octokit.issues.createComment(comment);
     }
 
-    // Initial comment
-    const comment = context.issue({
-            body: 'Cleanup is starting... '
-      });
-      await context.octokit.issues.createComment(comment);
 
     try {
         // Clean up resources
@@ -143,10 +135,10 @@ export default (app) => {
     } catch (error) {
         // Update comment with failure
         const comment = context.issue({
-          body: `Cleanup failed: ${error.message}`
+          body: 'Cleanup failed.'
         });
         await context.octokit.issues.createComment(comment);
-
+  
     }
   });
 };
